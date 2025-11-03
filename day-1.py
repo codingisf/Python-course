6729b386dc2dcaf6858588a123e7f3a968558211
Balaji <93272649+codingisf@users.noreply.github.com>
2025-11-03 21:56:37 +0530

    Add files via upload

---
+++ day-1.py
@@ -0,0 +1,57 @@
+# print("hello world")
+
+
+
+# Variable 
+
+
+# valid
+# a=10
+# _num = 10
+# Sum = 10
+# Num1 = 23
+
+# # inValid
+# 1high = 10      # Numeric comes at first
+# for = 23        # keyword cant be a variable
+# sum-of = 10     # sepical char cant take place in the variable name
+
+
+
+
+# print(Num)  
+
+
+
+# Data Types
+
+# 1.int             Ex : 10 , 20 , 44 , 9878
+# 2.bool            Ex : True , False
+# 3.str             Ex : "jeo" , 'laptop' , "lorem having the laptop" , '1221'
+# 4.Float           Ex : 10.5 , 12.5
+
+# print(type("10.0"))       # Type function is used to find a data type of the given value
+
+
+#Operators
+
+#Arithmatic                 # + ,- ,* ,/ ,%
+#Logical                    
+#Comparison or Relational   # < , > , <= , >= , == , !=
+#Assignment
+
+
+# a + b
+
+a = 2
+b = 10
+
+# print("ADD ->",a + b)
+# print("SUB ->",a - b)
+# print("MUL ->",a * b)
+# print("DIV ->",a / b)
+# print("MOD ->",a % b) # Remainder -> of division
+
+
+print(a != b)
+
