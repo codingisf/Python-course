4450b5090dafaae84af7ff017964fbe03e865b66
Balaji <93272649+codingisf@users.noreply.github.com>
2025-11-05 21:27:55 +0530

    Add files via upload

---
+++ day-2.py
@@ -0,0 +1,89 @@
+#operators
+
+a = 5
+
+b = 5
+
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)   # Reminder
+# print(a ** b)
+# print(a // b)
+
+
+# print(a<=b)    # < , > , <= , >= , == , !=
+
+# a = 10
+# b = 5
+
+# print(a > 3 and b < 10)   #True (both conditions true)
+# print(a > 15 or b < 10)   #True (one condition true)
+# print(not(a > 3))           #False (reverses the result)
+
+
+
+# assignement
+
+# a = 10
+
+# print(a)
+
+# a += 5   # a = 10 + 5
+
+# a = a + 5
+
+
+# print(a)
+
+
+#Type conversion
+
+
+# num = 5
+# print(type(num))
+
+
+# num = str('5')
+
+# print(type(num))
+
+
+# name = input("Enter your name : ")
+
+# print(type(name))
+# print("hello", name)
+
+# int a = "str";
+
+
+
+# a = int(input("enter a number :"))
+
+# print(a + 10)
+
+
+
+a = 10
+
+b = 20
+
+
+print(a+b)
+
+
+
+1num = 10
+
+
+a = 10
+
+b = 20
+
+
+print(a+b)
+
+
+
---
+++ day-3.py
@@ -0,0 +1,108 @@
+#Contional statement
+
+# a = 10
+
+# if a < 10 :
+#     print("This the True Block")
+#     print("This is second line")
+#     print("This the True Block")
+    
+# print("This is not a block")
+
+# Voting Program
+
+# age = 0
+
+# if age >= 18 :
+#     print("Your eligiable for Voting...")
+    
+# else :
+#     print("Your not eligiable for Voting...")
+
+
+# Grade system
+    
+# mark = 90
+
+# if mark >= 90:
+#     print("grade A")
+    
+# elif mark >= 70:
+#     print("grade B")
+
+# else:
+#     print("you  fail")
+
+
+# Nested if
+
+# age = 0
+
+# voter = False
+
+# if age >= 18 :
+#     if voter :
+#         print("Your eligiable for voting")
+#     else:
+#         print("You didn't Have a Voter ID")
+# else:
+#     print("Your age is Not eligiable")
+
+# if False:
+#     print("I'm True")
+    
+# else:
+#     print("I'm False")
+
+
+#Trenery Operator
+
+age = 0
+
+result = "your eligiable" if age >= 18 else "Not eligiable"
+
+print(result)
+
+
+# Task - 1
+
+# Mark 1 
+# Mark 2 
+# Mark 3
+
+# avg 
+
+# avg >=90  --> Grade 'A'
+# avg >=80  --> Grade 'B'
+# avg >=70  --> Grade 'c'
+# avg <70   --> Fail
+
+
+
+# largest number from three numbers
+
+# num 1 
+# num 2
+# num 3 
+
+# a = 70
+
+# b = 50
+
+# c = 100
+
+
+# if a > b:  #True
+    
+#     if a > c:
+#         print("A is First largest number")
+#     else:
+#         print("C is first Largest Number")
+# else:
+    
+#     if b > c :
+#         print("B is the Largest number")
+#     else:
+#         print("C is largest number")
+        
+    
