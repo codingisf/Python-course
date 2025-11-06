5b056099a70cb1cae35a776a38a54a50c180c797
Balaji <93272649+codingisf@users.noreply.github.com>
2025-11-06 21:34:01 +0530

    Add files via upload

---
+++ day-4.py
@@ -0,0 +1,51 @@
+#Looping
+
+
+# Range  # range(start , end , increament)
+
+# range = 1
+
+
+# for i in range(5,11):
+#     print(i)
+    #   print("hellow")
+
+# count = 7
+
+# while count <= 5 :
+#     print("Count is :",count)
+#     count+=1   # count = 5 + 1 -> 6
+    
+# print("The loop has ened")
+
+
+for i in range(1,11,1):      
+    if i == 5:
+        continue
+    print(i)
+    
+# print("The loop is ended")
+
+
+
+# j_angry = False
+# s_angry = True
+
+# if (j_angry == True and s_angry == True) or (j_angry == False and s_angry == False) :
+        
+#     print("True")
+        
+# else:
+        
+#     print("False")
+
+# N = 5
+
+# for i in range(1,11):
+#         print(i*N , end=" ")
+
+
+# for i in range(1,6):
+#     for j in range(1,i):
+#         print("*" , end=" ")
+#     print()
