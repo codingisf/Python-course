e36deb6cc96cf56ac279afea6996760dbbeb1c60
Balaji <93272649+codingisf@users.noreply.github.com>
2025-11-11 20:55:41 +0530

    Add files via upload

---
+++ day-7.py
@@ -0,0 +1,57 @@
+# List and Tuple
+
+# fruits = ["apple", "banana", "cherry"]
+
+
+# # fruits[1] = "mango"
+
+# # fruits.append("mango")            # this line use for add the mango in the last of the list
+# # fruits.insert(1,"mango")
+# # fruits.remove("apple")
+
+# # fruits.clear()
+
+# print(fruits)
+# # print(len(fruits))
+# # print(type(fruits))
+
+
+# # [0,1,2,3]
+
+
+# lst = ["apple",12,34.5,True,[2,4,"mangoo"]]
+# print(lst[4][2])
+
+
+# using list with the for loop
+
+# fruits = ["apple", "banana", "cherry"]
+
+# for i in fruits:
+#     print(i)
+
+
+# Tuple
+
+# tpl = ("apple", "banana", "cherry" , [1,"hello"])
+
+# tpl[0] = "mangoo"
+
+# tpl[3][1] = "world"
+
+# First we change the tuple into list for changing the value in tuple
+
+# a = list(tpl)
+
+# a[0] = "mangoo"
+
+# then after changing the value we returning the data type of tuple
+
+# b = tuple(a)
+
+# print(b)
+
+# print(type(b))
+
+
+# crtl + /
