a7a3e9dacad86ac32a0a32b18043443dca01fb2c
Balaji <93272649+codingisf@users.noreply.github.com>
2025-11-12 21:09:17 +0530

    Add files via upload

---
+++ day-8.py
@@ -0,0 +1,60 @@
+# doubt => how to store the valuse from user in list
+# a = []
+
+# for i in range(0,5):
+#     text = input("enter the value for list: ")
+#     a.append(text)
+    
+# print(a)
+
+# Dict
+
+# student = {
+#     "name": "Jeo",
+#     "age": 18,
+#     "course": "Python",
+#     "year":2025,
+# }
+# student["grade"] = "A"              # Key Value adding
+
+# student["age"] = 20                 # Value updating
+
+# del student["year"]                 # deleting key:Values
+# print(student)
+
+# print(student.keys())
+# print(student.values())
+# print(student.items())
+
+# for i,j in student.items():
+    
+#     print(i , ':' , j)
+    
+#     if i == "age":
+#         break
+
+
+# Sets
+
+# numbers = {1, 2, 3, 4, 5}
+
+# num = {1 ,2 ,9 , 10,11,12}
+
+# numbers.add(6)
+
+# numbers.remove(3)
+
+# numbers.pop()
+
+
+
+# print(numbers.union(num))
+
+# print(numbers.intersection(num))
+
+# print(numbers.difference(num))
+
+
+
+
+
